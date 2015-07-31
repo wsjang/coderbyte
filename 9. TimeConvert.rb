8e949364d4ecf2ed9c8bac32f9aad6eff52be468
wsjang <wsjang34@gmail.com>
2015-07-31 15:15:45 +0900

    Create 9. TimeConvert.rb

---
+++ 9. TimeConvert.rb
@@ -0,0 +1,30 @@
+def TimeConvert(num)
+
+  # code goes here
+  
+  return (num/60).to_s + ":" + (num%60).to_s
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
