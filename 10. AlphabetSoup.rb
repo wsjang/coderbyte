6a7f5b20ff69799de5f9dd8c99c3aa652730e667
wsjang <wsjang34@gmail.com>
2015-07-31 15:25:18 +0900

    Create 10. AlphabetSoup.rb

---
+++ 10. AlphabetSoup.rb
@@ -0,0 +1,10 @@
+def AlphabetSoup(str)
+
+  # code goes here
+  return str.split("").sort.join("")
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)           
