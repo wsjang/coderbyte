2207a30a7bb63d0955f97ea5b4ee63c9c1bdfae4
wsjang <wsjang34@gmail.com>
2015-07-31 16:06:40 +0900

    Create 15. ABCheck.rb

---
+++ 15. ABCheck.rb
@@ -0,0 +1,10 @@
+def ABCheck(str)
+
+  # code goes here
+  return str.scan(/a...b/).count == 3 ? true : false
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ABCheck(STDIN.gets)           
